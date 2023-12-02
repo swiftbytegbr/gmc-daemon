0ea8956dbcccad28005d717eddbb2e4ee8a669b1
Hufeisen <hufeisen@hufeisen-games.de>
2023-12-02 19:22:54 +0100

    feat: server update packet consumer

---
+++ src/main/java/de/swiftbyte/gmc/stomp/consumers/server/UpdateServerVersionConsumer.java
@@ -0,0 +1,25 @@
+package de.swiftbyte.gmc.stomp.consumers.server;
+
+import de.swiftbyte.gmc.packet.server.ServerSettingsPacket;
+import de.swiftbyte.gmc.packet.server.ServerUpdatePacket;
+import de.swiftbyte.gmc.server.GameServer;
+import de.swiftbyte.gmc.stomp.StompPacketConsumer;
+import de.swiftbyte.gmc.stomp.StompPacketInfo;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@StompPacketInfo(path = "/user/queue/server/update", packetClass = ServerUpdatePacket.class)
+public class UpdateServerVersionConsumer implements StompPacketConsumer<ServerUpdatePacket> {
+
+    @Override
+    public void onReceive(ServerUpdatePacket packet) {
+        log.info("Updating server with id " + packet.getServerId() + ".");
+        GameServer server = GameServer.getServerById(packet.getServerId());
+
+        if(server != null) {
+            server.installServer();
+        } else {
+            log.error("Server with id " + packet.getServerId() + " not found!");
+        }
+    }
+}
