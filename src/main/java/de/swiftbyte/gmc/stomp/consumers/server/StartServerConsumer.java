99326e99da46b4303417ad01163132b9a3ca2130
Hufeisen <hufeisen@hufeisen-games.de>
2023-12-02 13:12:28 +0100

    feat: consume start server

---
+++ src/main/java/de/swiftbyte/gmc/stomp/consumers/server/StartServerConsumer.java
@@ -0,0 +1,25 @@
+package de.swiftbyte.gmc.stomp.consumers.server;
+
+import de.swiftbyte.gmc.packet.node.NodeLoginAckPacket;
+import de.swiftbyte.gmc.packet.server.ServerStartPacket;
+import de.swiftbyte.gmc.server.GameServer;
+import de.swiftbyte.gmc.stomp.StompPacketConsumer;
+import de.swiftbyte.gmc.stomp.StompPacketInfo;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@StompPacketInfo(path = "/user/queue/server/start", packetClass = ServerStartPacket.class)
+public class StartServerConsumer implements StompPacketConsumer<ServerStartPacket> {
+
+    @Override
+    public void onReceive(ServerStartPacket packet) {
+        log.info("Starting server with id " + packet.getServerId() + ".");
+        GameServer server = GameServer.getServerById(packet.getServerId());
+
+        if(server != null) {
+            server.start();
+        } else {
+            log.error("Server with id " + packet.getServerId() + " not found!");
+        }
+    }
+}
