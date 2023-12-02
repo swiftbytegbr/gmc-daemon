aae49866db63efb7bcf33860d5fd4073a44b2498
Hufeisen <hufeisen@hufeisen-games.de>
2023-12-02 19:28:11 +0100

    feat: send rcon command packet consumer

---
+++ src/main/java/de/swiftbyte/gmc/stomp/consumers/server/SendRCONPacketConsumer.java
@@ -0,0 +1,32 @@
+package de.swiftbyte.gmc.stomp.consumers.server;
+
+import de.swiftbyte.gmc.packet.server.ServerRconPacket;
+import de.swiftbyte.gmc.packet.server.ServerRconResponsePacket;
+import de.swiftbyte.gmc.packet.server.ServerStartPacket;
+import de.swiftbyte.gmc.server.GameServer;
+import de.swiftbyte.gmc.stomp.StompPacketConsumer;
+import de.swiftbyte.gmc.stomp.StompPacketInfo;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@StompPacketInfo(path = "/user/queue/server/rcon", packetClass = SendRCONPacketConsumer.class)
+public class SendRCONPacketConsumer implements StompPacketConsumer<ServerRconPacket> {
+
+    @Override
+    public void onReceive(ServerRconPacket packet) {
+        log.info("Sending RCON command to server with id " + packet.getServerId() + ".");
+        GameServer server = GameServer.getServerById(packet.getServerId());
+
+        if(server != null) {
+            String response = server.sendRconCommand(packet.getCommand().getCommand());
+            packet.getCommand().setResponse(response);
+
+            ServerRconResponsePacket responsePacket = new ServerRconResponsePacket();
+            responsePacket.setServerId(packet.getServerId());
+            responsePacket.setCommand(responsePacket.getCommand());
+
+        } else {
+            log.error("Server with id " + packet.getServerId() + " not found!");
+        }
+    }
+}
