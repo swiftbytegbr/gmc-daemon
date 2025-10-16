9b8a21f303f280ffae59dc5eefa5b8e2ce623c5d
Hufeisen <hufeisen@hufeisen-games.de>
2025-10-16 19:09:50 +0200

    feat(GMC-78): disable auto update when daemon version is DEV

--- src/main/java/de/swiftbyte/gmc/daemon/stomp/consumers/node/NodeUpdatePacketConsumer.java
+++ src/main/java/de/swiftbyte/gmc/daemon/stomp/consumers/node/NodeUpdatePacketConsumer.java
@@ -1,6 +1,6 @@
 package de.swiftbyte.gmc.daemon.stomp.consumers.node;
 
-import de.swiftbyte.gmc.daemon.Node;
+import de.swiftbyte.gmc.daemon.*;
 import de.swiftbyte.gmc.common.packet.node.NodeUpdatePacket;
 import de.swiftbyte.gmc.daemon.stomp.StompPacketConsumer;
 import de.swiftbyte.gmc.daemon.stomp.StompPacketInfo;
@@ -16,7 +16,7 @@ public class NodeUpdatePacketConsumer implements StompPacketConsumer<NodeUpdateP
         if (packet.isAutoUpdate()) {
 
             log.info("New daemon version available: {}", packet.getVersion());
-            if (Node.INSTANCE.isAutoUpdateEnabled()) {
+            if (Node.INSTANCE.isAutoUpdateEnabled() && !Application.getVersion().contains("DEV")) {
                 log.info("Auto update enabled. Updating...");
                 Node.INSTANCE.updateDaemon();
             } else {
