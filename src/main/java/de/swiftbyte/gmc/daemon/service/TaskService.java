8163407d4f78989443c81294a4c6080af67f3147
Jona Faust <jona.faust@gmx.de>
2025-12-23 13:20:27 +0100

    Revert "feat: implement savegame wipe functionality"
    This reverts commit 3d2d1eb9ca4211a27bf4b2637592797521b2bf6d.

--- src/main/java/de/swiftbyte/gmc/daemon/service/TaskService.java
+++ src/main/java/de/swiftbyte/gmc/daemon/service/TaskService.java
@@ -46,9 +46,6 @@ public class TaskService {
         registerConsumer(NodeTask.Type.TIMED_SHUTDOWN, new TimedShutdownTaskConsumer());
         registerConsumer(NodeTask.Type.TIMED_RESTART, new TimedRestartTaskConsumer());
 
-        // Server maintenance
-        registerConsumer(NodeTask.Type.SAVEGAME_WIPE, new de.swiftbyte.gmc.daemon.tasks.consumers.SavegameWipeTaskConsumer());
-
     }
 
     public static void shutdownTaskService() {
